72419ab54fd430cf38bb52efd3bf9085fa1b1ec7
Chyngyz0909 <bbegemotov_2002@inbox.ru>
2021-01-09 22:49:52 +0600

    hbh

--- app/src/main/java/com/example/android1hw11/MainActivity.java
+++ app/src/main/java/com/example/android1hw11/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.android1hw11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    Button btn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn= findViewById(R.id.enter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Заполнить данные", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+        });
+
+
     }
 }
